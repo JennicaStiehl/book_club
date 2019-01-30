9c7e700f56c408a070d8d22b705ceaee5c13fd88
Chris Lewis <13chlewis@gmail.com>
2019-01-29 17:54:21 -0700

    Finished writing test for User Story 6

--- spec/features/book_index_spec.rb
+++ spec/features/book_index_spec.rb
@@ -2,8 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'when visitor visits book index page', type: :feature do
   it 'can see all titles and attributes of each book in the database' do
-    book_1 = Book.create(title: 'House of Leaves', pages: 709, year: 2000)
     author_1 = Author.create(name: 'Mark Z. Danielewski')
-    AuthorBook.create(book_id: book_1.id, author_id: author_1.id)
+    author_2 = Author.create(name: 'Terry Pratchett')
+    author_3 = Author.create(name: 'Neil Gaiman')
+    book_1 = author_1.books.create(title: 'House of Leaves', pages: 709, year: 2000)
+    book_2 = Book.create(title: 'Good Omens', pages: 288, year: 1990, authors: [author_2, author_3])
+
+    visit '/books'
+
+    within "#book-#{book_1.id}" do
+      expect(page).to have_content(book_1.title)
+      expect(page).to have_content(book_1.pages)
+      expect(page).to have_content(book_1.year)
+      expect(page).to have_content(book_1.authors)
+      expect(page).to_not have_content(book_2.title)
+    end
+
+    within "#book-#{book_2.id}" do
+      expect(page).to have_content(book_2.title)
+      expect(page).to have_content(book_2.pages)
+      expect(page).to have_content(book_2.year)
+      expect(page).to have_content(book_2.authors)
+      expect(page).to_not have_content(book_1.title)
+    end
   end
 end
