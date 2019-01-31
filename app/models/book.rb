c3c2ba49880cc768a96dc54f1194768d79106832
Chris Lewis <13chlewis@gmail.com>
2019-01-30 17:09:08 -0700

    User Story 8 Tests written, sort by pages functionality working
    Co-authored-by: Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,22 @@ class Book < ApplicationRecord
   has_many :author_books
   has_many :authors, through: :author_books
   has_many :reviews
+
+  def self.sort_by_pages
+    Book.order(:pages)
+  end
+
+  def self.sort_by_pages_desc
+    Book.order(pages: :desc)
+  end
+
+  def self.sort_by_rating
+    binding.pry
+  end
+
+  def self.sort_by_rating_desc
+  end
+
+  def self.sort_by_review_number
+  end
 end
