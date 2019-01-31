c99223aee18b3cdf9240107b1d445627c87d3761
JennicaStiehl <jennica.stiehl@gmail.com>
2019-01-31 15:23:56 -0700

    sort by  average rating by book, number of reviews, pages
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,25 +3,16 @@ class Book < ApplicationRecord
   has_many :authors, through: :author_books
   has_many :reviews
 
-  def self.sort_by_pages
-    Book.order(:pages)
+  def self.sort_by_pages(order)
+    Book.order(pages: :order)
   end
 
-  def self.sort_by_pages_desc
-    Book.order(pages: :desc)
+  def self.sort_by_rating(order)
+    Book.select('books.*, avg(reviews.rating) as avg_rating').left_outer_joins(:reviews).group('books.id').order("avg_rating #{order}")
   end
 
-  def self.sort_by_rating
-    # binding.pry
-    Book.select('books.id, avg(reviews.rating)').joins(:reviews).group('books.id').order('avg(review.rating)')
+  def self.sort_by_review_number(order)
+    Book.select('books.*, count(reviews.rating) as num_rating').left_outer_joins(:reviews).group('books.id').order("num_rating #{order}") 
   end
 
-  def self.sort_by_rating_desc
-  end
-
-  def self.sort_by_review_number
-  end
-
-  def self.sort_by_review_number_desc
-  end
 end
