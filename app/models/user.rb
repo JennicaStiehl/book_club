7c5443af3ffc921af6d495898d691fcf01f77201
JennicaStiehl <jennica.stiehl@gmail.com>
2019-02-02 15:31:20 -0700

    welcome page, welcome controller
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :reviews
+
+  def sort_by_newest
+    Reviews.order(created_at: :desc)
+  end
+
+  def sort_by_oldest
+    Reviews.order(created_at: :asc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root: '/'
-  
+root  'welcome#index'
+
   resources :books do
     resources :reviews
   end
