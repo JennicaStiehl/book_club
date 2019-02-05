383f6436681c21c618e5552465076056b1749b91
Chris Lewis <13chlewis@gmail.com>
2019-02-05 10:16:27 -0700

    Book index tests nearly all passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if params[:sort] == 'oldest'
-      binding.pry
       @reviews = @user.sort_by_oldest
     elsif params[:sort] == 'newest'
       @reviews = @user.sort_by_newest
