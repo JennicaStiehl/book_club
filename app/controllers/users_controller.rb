84811c0793f0b34c13eb38e9e0cb5c238584bc30
Chris Lewis <13chlewis@gmail.com>
2019-02-05 09:36:50 -0700

    Basic styling and layout for user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if params[:sort] == 'oldest'
+      binding.pry
       @reviews = @user.sort_by_oldest
     elsif params[:sort] == 'newest'
       @reviews = @user.sort_by_newest
